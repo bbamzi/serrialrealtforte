7128fef882a6b2b3840fa7baa4622723f1425b98
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-07-11 23:55:16 +0100

    {+}

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from .models import *
+
+class paymentAdmin(admin.ModelAdmin):
+    model= PaymentDetail
+    list_display = ('payment_type', 'payment_value')
+
+class bookingAdmin(admin.ModelAdmin):
+    model= Booking
+class HostingAdmin(admin.ModelAdmin):
+    model= HostingAddress
+    list_display = ('address_nickname', 'address',"lat","lng")
+   
+class TestimonyAdmin(admin.ModelAdmin):
+    model= Testimonial
+    list_display = ('name', 'social_username',"social_network","testimony","ratings","url")
+class ActiveAddressAdmin(admin.ModelAdmin):
+    model= ActiveAddress
+    
+   
+
+
 # Register your models here.
+admin.site.register(PaymentDetail, paymentAdmin)
+admin.site.register(Booking, bookingAdmin)
+admin.site.register(HostingAddress, HostingAdmin)
+admin.site.register(Testimonial, TestimonyAdmin)
+admin.site.register(ActiveAddress, ActiveAddressAdmin)
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-]
+    path('', views.index, name='index'),
+    path('receipt-upload', views.updateReceipt, name='receipt-upload'),
+    path('appointments', views.appointments, name='appointments'),
+    path('track-order', views.track, name='track-order'),
+    path('cancel-order', views.cancel_order, name='cancel-order'),
+    path('testimonials', views.testimonials, name='testimonials')
+    
+    ]
