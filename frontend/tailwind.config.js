1bd56fdf1961e98aca1da42c59b13364f3b22255
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-10-26 16:00:33 +0100

    header

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+    },
+  },
+  plugins: [],
+};
