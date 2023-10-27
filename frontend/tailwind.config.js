3684568a7c1f43b062c5e7172e704b2b3aa8816b
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-10-27 17:30:08 +0100

    {+}

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        tipper:
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/tipper.jpg')",
+        survey:
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/survey.jpg')",
+        land_survey:
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/land_survey.jpg')",
+        contract:
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/contract.webp')",
+      },
+    },
   },
   plugins: [],
 };
