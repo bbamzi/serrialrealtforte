49f3b3d1ddee16c63b2f1a54a64d81c496f80b2c
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-10-28 00:32:54 +0100

    {+}

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
     extend: {
       backgroundImage: {
         tipper:
-          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../../../frontend/src/img/tipper.jpg')",
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/tipper.jpg')",
         survey:
-          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../../../frontend/src/img/survey.jpg')",
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/survey.jpg')",
         land_survey:
-          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../../../frontend/src/img/land_survey.jpg')",
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/land_survey.jpg')",
         contract:
-          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../../../frontend/src/img/contract.webp')",
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/contract.webp')",
       },
     },
   },
