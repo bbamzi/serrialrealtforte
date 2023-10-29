557e89d74587ecf8cbeff724b684c6a1817daaee
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-10-29 15:25:41 +0100

    {+}

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
         tipper:
           "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/tipper.jpg')",
         survey:
-          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/survey.jpg')",
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/surveyor.jpeg')",
         land_survey:
           "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/land_survey.jpg')",
         contract:
-          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/contract.webp')",
+          "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/land_docs.jpeg')",
+        about_us: "url('./../img/circle-scatter-haikei.svg')",
+        reviews: "url('./../img/haikei.svg')",
       },
     },
   },
