a26b284e9fd85a8849ef07684857dc88185c91a1
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-11-01 17:36:37 +0100

    {+}

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
           "linear-gradient(to right bottom, rgba(16, 106, 31, 0.6), rgba(0, 0, 0, 0.6)), url('./../img/land_docs.jpeg')",
         about_us: "url('./../img/circle-scatter-haikei.svg')",
         reviews: "url('./../img/haikei.svg')",
+        map: "  background: linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))",
       },
     },
   },
