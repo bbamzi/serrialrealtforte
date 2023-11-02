b46cec704de5913c47c29fcaf4d10c9d58c667cb
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-11-02 06:25:42 +0100

    {-}

--- backend/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.maintenance, name='maintenance'),
-    path('index', views.index, name='index'),
-   
-    
-    ]
